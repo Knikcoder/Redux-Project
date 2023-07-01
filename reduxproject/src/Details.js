340fd177659d3fb74a107239785f74b3379ff526
Kumar Raja <kumarrajabammidi@gmail.com>
2023-07-01 13:58:35 +0530

    Kumar Raja.maincommit

---
+++ reduxproject/src/Details.js
@@ -0,0 +1,103 @@
+import React from 'react'
+import './Details.css'
+function Details() {
+  return (
+    <div className='overall'>
+    <div>
+    <div className='head-details'>
+        <h2>Edit Your Educational Profile</h2>
+    </div>
+    <div className='main'>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+    </div>
+    <div className='main'>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+    </div>
+    </div>
+
+    <div>
+    <div className='head-details'>
+    <h2>Additional Details</h2>
+        <p>Note: <span>Any detail that are not verified yet contribute to profile strength.</span></p>
+    </div>
+    <div className='main'>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+    </div>
+    <div className='main'>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+    </div>
+    <div className='main'>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+    </div>
+    <div className='Doc-sub'>
+        <input type='file' place/>
+    </div>
+    </div>
+    <div>
+    <div className='head-details'>
+        <h2>Edit Your Educational Profile</h2>
+    </div>
+    <div className='main'>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+    </div>
+    <div className='main'>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+        <div className='item'>
+            <p>Educational Details</p>
+            <input type='text'/>
+        </div>
+    </div>
+    <div className='Doc-sub'>
+        <input type='file' place/>
+    </div>
+    </div>
+    </div>
+  )
+}
+
+export default Details
